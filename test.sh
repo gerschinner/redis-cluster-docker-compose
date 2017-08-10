f277b970b7affa5f03214007ced44d29c299a86b
Germán Ignacio Schinner <german.schinner@grey.com>
2017-08-10 15:01:38 -0300

    Upload Test Files

--- test.sh
+++ test.sh
@@ -1,32 +1,47 @@
-MASTER_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redisclusterdockercompose_master_1)
-SLAVE_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redisclusterdockercompose_slave_1)
-SENTINEL_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redisclusterdockercompose_sentinel_1)
-
-echo Redis master: $MASTER_IP
-echo Redis Slave: $SLAVE_IP
+SERVER_IP0=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redisclusterdockercompose_master_1)
+SERVER_IP1=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redisclusterdockercompose_slave_1)
+SERVER_IP2=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redisclusterdockercompose_slave_2)
+SENTINEL_IP1=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redisclusterdockercompose_sentinel_1)
+SENTINEL_IP2=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redisclusterdockercompose_sentinel_2)
+SENTINEL_IP3=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redisclusterdockercompose_sentinel_3)
 echo ------------------------------------------------
-echo Initial status of sentinel
+echo Redis Master: $SERVER_IP0
+echo Redis Slave Replica: $SERVER_IP1
+echo Redis Slave Replica: $SERVER_IP2
+echo Redis Sentinel_1: $SENTINEL_IP1
+echo Redis Sentinel_2: $SENTINEL_IP2
+echo Redis Sentinel_3: $SENTINEL_IP3
 echo ------------------------------------------------
+echo "- Current sentinel status -"; echo ""
 docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 info Sentinel
-echo Current master is
+echo ""; echo "# Current master"
 docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
 echo ------------------------------------------------
-
-echo Stop redis master
+echo "Stopping redis current master (60 seconds)";echo ""
 docker pause redisclusterdockercompose_master_1
-echo Wait for 10 seconds
-sleep 10
-echo Current infomation of sentinel
-docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 info Sentinel
-echo Current master is
-docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
-
-
+sleep 60
 echo ------------------------------------------------
-echo Restart Redis master
-docker unpause redisclusterdockercompose_master_1
-sleep 5
-echo Current infomation of sentinel
+echo "- Current sentinel status -"; echo ""
 docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 info Sentinel
-echo Current master is
+echo ""; echo "# Current master"
 docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
+echo ------------------------------------------------
+echo "Restarting redis previous master as a slave & stopping redis current master (60 seconds)";echo ""
+docker unpause redisclusterdockercompose_master_1
+docker pause redisclusterdockercompose_slave_1
+sleep 60
+echo ------------------------------------------------
+echo "- Current sentinel status -"; echo ""
+docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 info Sentinel
+echo ""; echo "# Current master"
+docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
+echo ------------------------------------------------
+echo "Restarting redis prevoius master as current slave (60 seconds)"; echo ""
+docker unpause redisclusterdockercompose_slave_1
+sleep 60
+echo ------------------------------------------------
+echo "- Current sentinel status -"; echo ""
+docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 info Sentinel
+echo ""; echo "# Current master"
+docker exec redisclusterdockercompose_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
+echo ------------------------------------------------
